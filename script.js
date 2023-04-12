f3ae17350734ca70ce7c53bf58049a7b44ea51a6
thomascaldana <thomascaldana@gmail.com>
2023-04-12 15:55:56 -0300

    html done

---
+++ script.js
@@ -0,0 +1,9 @@
+/* var container = document.querySelector(".header_class");
+
+var a = document.getElementsByClassName("header_class");
+
+container.removeChild(a); */
+
+/* var divHeader = document.querySelector(".header_class");
+
+divHeader.remove() */
