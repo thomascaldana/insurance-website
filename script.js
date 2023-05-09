c3c66a8022b37590cb865d68e7ecceedaefff2c7
thomascaldana <thomascaldana@gmail.com>
2023-05-09 09:30:38 -0300

    removed useless code in js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-/* function showMenu() {
-    let titleModification = document.querySelector('.main_title').innerHTML = "NOVO TITULOO"
-    let menuButton = document.querySelector('.img_menu')
-    let bodyColor = document.getElementsByTagName('body')
-    menuButton.
-    bodyColor.style.color = "red"
-    titleModification.innerHTML = "NOVO TITULO"
-}
 
-addEventListener("click", showMenu)
-
-let titleModification = document.querySelector('.main_title')
-titleModification.innerHTML = "NOVO TITULO"
-
-document.querySelector('.main_title').innerHTML = "NOVO TITULOO"
- */
 
 function mudarConteudo() {
     let opened_menu = document.querySelector('.opened_menu');
